e1204184607921302c7c56b6cd5afddb26f37a53
thiyagu06 <thiyagu103@gmail.com>
2020-01-23 15:23:50 +0100

    added logic fetch dlq messages

--- src/test/kotlin/com/thiyagu/reactive/sqs/SqsAccessorTest.kt
+++ src/test/kotlin/com/thiyagu/reactive/sqs/SqsAccessorTest.kt
@@ -4,7 +4,6 @@ import com.thiyagu.reactive.domain.MessageDecorator
 import com.thiyagu.reactive.domain.SqsConfig
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
@@ -78,6 +77,33 @@ class SqsAccessorTest {
         coEvery { sqsAsyncClient.deleteMessage(deleteMessageRequest) } returns response
         sqsAccessor.deleteMessage(MessageDecorator(true, message))
         coVerify(exactly = 1) { sqsAsyncClient.deleteMessage(deleteMessageRequest) }
+    }
 
+    @Test
+    fun `validate able fetch dlq sqs messages`() = runBlockingTest {
+        val receiveMessageRequest = ReceiveMessageRequest
+            .builder()
+            .queueUrl("DEAD_QUEUE_URL")
+            .maxNumberOfMessages(10)
+            .waitTimeSeconds(20)
+            .build()
+
+        val message = Message
+            .builder()
+            .body("HelloWorld")
+            .build()
+
+        val response = CompletableFuture.completedFuture(
+            ReceiveMessageResponse
+                .builder()
+                .messages(message)
+                .build()
+        )
+
+        coEvery { sqsAsyncClient.receiveMessage(receiveMessageRequest) } returns response
+        val fetchedMessages = sqsAccessor.fetchDlqMessages()
+        coVerify(exactly = 1) { sqsAsyncClient.receiveMessage(receiveMessageRequest) }
+        assertEquals(1, fetchedMessages.size)
+        assertEquals(message, fetchedMessages[0])
     }
 }
