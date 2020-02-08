aca8bb077b56cda1f9cbe75081121196bf3b7e36
Thiyagu GK <thiyagu103@gmail.com>
2020-02-08 22:26:34 +0100

    flexibility to define custom thread pool for sqs listener

--- src/main/kotlin/com/thiyagu/reactive/SqsListener.kt
+++ src/main/kotlin/com/thiyagu/reactive/SqsListener.kt
@@ -6,16 +6,18 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.cancel
+import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import kotlin.coroutines.CoroutineContext
 
-class SqsListener(private val defaultMessageProcessor: DefaultMessageProcessor) : CoroutineScope,
+class SqsListener(private val defaultMessageProcessor: DefaultMessageProcessor,
+                  private val threadPool: ExecutorService = Executors.newFixedThreadPool(2)) : CoroutineScope,
     MessageListener {
 
     private val job = Job()
 
     override val coroutineContext: CoroutineContext
-        get() = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
+        get() = threadPool.asCoroutineDispatcher()
 
     private val scope = CoroutineScope(job + coroutineContext)
 
